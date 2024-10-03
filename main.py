1aa5d5dc41bb1421f42d2f8e90831e3209bf3e35
Gina Kim <gina261@yonsei.ac.kr>
2024-10-04 03:03:09 +0900

    test

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="🍊참신한 제주 맛집!")
+st.set_page_config(page_title="🍊🍊🍊")
+
+st.title("Hello Ybigta👋")
+st.subheader("Ybigta Team DA's Bigcontest")
