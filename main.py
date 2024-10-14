92fb24cd1c54d0ce5d485372c429fc60ac7a85a5
Gina Kim <gina261@yonsei.ac.kr>
2024-10-14 13:55:21 +0900

    streamlit - gemini 설정

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ import streamlit as st
 # Gemini 설정
 import google.generativeai as genai
 
+# gemini 설정
+GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
+genai.configure(api_key=GOOGLE_API_KEY)
+
+# Gemini 모델 선택
+model = genai.GenerativeModel("gemini-1.5-flash")
+
 st.set_page_config(page_title="🍊🍊🍊")
 
 st.title("Hello Ybigta👋")
