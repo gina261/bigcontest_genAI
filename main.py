d3335cec7f352d950ec635ac65809fc1d695fc2d
Gina Kim <gina261@yonsei.ac.kr>
2024-10-16 15:18:54 +0900

    1016

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
-# CSS for adding a full-width background image
+# CSS for adding a full-width background image with transparency
 st.markdown(
     """
     <style>
     @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap');
 
     .full-width-banner {
-        background-image: url('https://ifh.cc/g/lD0gY1.jpg'); /* 배경으로 사용할 이미지 URL */
+        position: relative;
+        background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://i.ibb.co/0tVV9zh'); /* 반투명 검정색 오버레이와 이미지 */
         background-size: cover; /* 이미지 크기를 전체 영역에 맞춤 */
         background-position: center; /* 이미지 중앙 정렬 */
         height: 500px; /* 배너의 높이 설정 */
@@ -17,7 +18,6 @@ st.markdown(
         align-items: center;
         justify-content: center;
         color: white;
-        position: relative;
         z-index: 1;
         flex-direction: column; /* 세로 방향 정렬을 위한 추가 */
     }
@@ -48,12 +48,7 @@ st.markdown(
     """
     <div class="full-width-banner">
         <h1>Welcome to team 예쁘DA</h1>
-        <h2>상황 맞춤형 제주 맛집 찾기를 돕습니다.</h2>
     </div>
     """,
     unsafe_allow_html=True
 )
-
-# Sample content
-st.write("This is the content of the app below the banner.")
-st.write("You can add more Streamlit components here.")
