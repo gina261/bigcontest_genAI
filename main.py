8ba1054e2084d4ee6b925b6cd1454098bce8f8fb
Gina Kim <gina261@yonsei.ac.kr>
2024-10-16 14:46:28 +0900

    1016

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ st.markdown(
         background-image: url('https://ifh.cc/g/lD0gY1.jpg'); /* 배경으로 사용할 이미지 URL */
         background-size: cover; /* 이미지 크기를 전체 영역에 맞춤 */
         background-position: center; /* 이미지 중앙 정렬 */
-        height: 400px; /* 배너의 높이 설정 */
+        height: 500px; /* 배너의 높이 설정 */
         width: 100vw; /* 페이지의 전체 너비를 사용 */
         margin-left: calc(-50vw + 50%); /* 페이지 중앙 정렬 후 왼쪽으로 이동 */
         display: flex;
@@ -17,6 +17,7 @@ st.markdown(
         color: white;
         position: relative;
         z-index: 1;
+        flex-direction: column; /* 세로 방향 정렬을 위한 추가 */
     }
 
     /* 텍스트 스타일 */
@@ -27,6 +28,15 @@ st.markdown(
         color: white;
         text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); /* 텍스트에 그림자 추가 */
     }
+
+    .full-width-banner h2 {
+        font-size: 2em;
+        font-family: 'Karla', sans-serif;
+        margin: 0;
+        color: white;
+        text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); /* 텍스트에 그림자 추가 */
+        margin-top: 10px; /* 상단 마진 추가 */
+    }
     </style>
     """,
     unsafe_allow_html=True
@@ -36,7 +46,8 @@ st.markdown(
 st.markdown(
     """
     <div class="full-width-banner">
-        <h1>Welcome to My Streamlit App</h1>
+        <h1>Welcome to team 예쁘DA</h1>
+        <h2>제주 맛집 추천 챗봇</h2>
     </div>
     """,
     unsafe_allow_html=True
