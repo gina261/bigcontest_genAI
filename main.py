9389a4196cc1820a7abad22a71c1b8bdae0af16e
Gina Kim <gina261@yonsei.ac.kr>
2024-10-19 05:14:50 +0900

    1019

--- main.py
+++ main.py
@@ -310,3 +310,13 @@ st_data = st_folium(jeju_map, width=800, height=600)
 if st_data and st_data.get('last_active_drawing'):
     selected_region = st_data['last_active_drawing']['properties']['adm_nm']
     st.write(f"Selected Region: {selected_region}")
+    
+    
+st.markdown(
+    """
+    <div class="centered-subtext">
+        오늘의 기분이나 상황을 입력해주세요. 그에 맞는 제주의 멋진 곳을 추천해드립니다.
+    </div>
+    """,
+    unsafe_allow_html=True
+)
