c525044e69a4d59f551eeac3b0502a29b4ab405c
Gina Kim <gina261@yonsei.ac.kr>
2024-10-19 02:41:06 +0900

    1019

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ st.markdown(
         text-align: center;
         color: white;
         width: 100%;
-        padding-top: 15px;
+        padding-top: 17px;
         padding-left: 40px
     }
 
