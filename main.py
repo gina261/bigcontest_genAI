302fb0b46d5de0a5598017f48a64a5c6d3db15b9
Gina Kim <gina261@yonsei.ac.kr>
2024-10-19 05:22:12 +0900

    1019

--- main.py
+++ main.py
@@ -259,7 +259,7 @@ st.markdown(
     
 
 # 제주도 중심 좌표
-jeju_center = [33.38, 126.52] # 기존 33.4996, 126.5312
+jeju_center = [33.38, 126.54] # 기존 33.4996, 126.5312
 
 mapbox_token = st.secrets["MAPBOX_API_KEY"]
 
