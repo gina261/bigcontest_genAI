5822beac9f6fc6a36adc1edd1be52b9aeb5f13b1
Gina Kim <gina261@yonsei.ac.kr>
2024-10-19 17:29:54 +0900

    1019

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ st.markdown(
         background-image: url('https://github.com/gina261/bigcontest_genAI/blob/main/images/banner.png?raw=true');
         background-size: cover; /* 이미지 크기를 전체 영역에 맞춤 */
         background-position: center;
-        height: 460px; /* 배너의 높이 설정 */
+        height: 470px; /* 배너의 높이 설정 */
         width: 100vw; /* 페이지의 전체 너비를 사용 */
         margin-left: calc(-50vw + 50%); /* 페이지 중앙 정렬 후 왼쪽으로 이동 */
         display: flex;
