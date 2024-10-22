417683889036f0c6c5717f065ce57e7fa91b06bd
Gina Kim <gina261@yonsei.ac.kr>
2024-10-22 19:29:12 +0900

    1022

--- main.py
+++ main.py
@@ -559,7 +559,7 @@ elif st.session_state.page == 'next_page':
             position: fixed;
             bottom: 30px;
             left: 20%;
-            width: 60%;
+            width: 70%;
             height: 50px;
             background-color: #fff;
             padding: 10px 25px;
