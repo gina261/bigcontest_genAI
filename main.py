927386925ddcfbd79256618c710696611c0d0b0e
Gina Kim <gina261@yonsei.ac.kr>
2024-10-24 02:32:30 +0900

    1023

--- main.py
+++ main.py
@@ -592,6 +592,7 @@ elif st.session_state.page == 'next_page':
             background-color: #ffffff;
             border-color: #ffffff;
             border-radius: 20px;
+            padding: 10px 20px;
         }
         div [data-testid="stChatInput"] {
             background-color: #ffefcc;
