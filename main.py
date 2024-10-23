cd54bd35167a744386059146148325e725c85ac5
Gina Kim <gina261@yonsei.ac.kr>
2024-10-23 20:51:38 +0900

    1023

--- main.py
+++ main.py
@@ -565,7 +565,7 @@ elif st.session_state.page == 'next_page':
         div[data-testid="stMarkdownContainer"] {
             background-color: #ffffff;
             padding: 10px 20px;
-            margin-bottom: 0;
+            margin-bottom: 0px;
         }
         
         
