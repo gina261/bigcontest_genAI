a5968b5c09152070a325bc24d930d2147262c073
Gina Kim <gina261@yonsei.ac.kr>
2024-10-24 03:11:45 +0900

    1023

--- main.py
+++ main.py
@@ -379,7 +379,7 @@ if st.session_state.page == 'main':
     # 선택 초기화 버튼 클릭 시 선택된 지역 초기화
     if st.button("선택 초기화"):
         st.session_state.selected_regions = []
-        st.experimental_rerun()
+        st.experimental_set_query_params(reset="true")  # 페이지 강제 새로고침
         
     # 선택된 지역 텍스트를 위한 placeholder 생성
     selected_region_text = st.empty()
