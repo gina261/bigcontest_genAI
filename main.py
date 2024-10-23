cf9ef3917280416348017f864a2e6491de574b98
Gina Kim <gina261@yonsei.ac.kr>
2024-10-23 21:43:03 +0900

    1023

--- main.py
+++ main.py
@@ -580,7 +580,7 @@ elif st.session_state.page == 'next_page':
         }
         
         div[data-testid="stMarkdown"] p {
-            min-width: 50px;
+            font-family: 'Pretendard', sans-serif;
         }
         
         </style>
