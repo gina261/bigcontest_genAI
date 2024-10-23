9eece2715fb8b0f7aaf3fe3acb947486c352317b
Gina Kim <gina261@yonsei.ac.kr>
2024-10-24 03:19:23 +0900

    1023

--- main.py
+++ main.py
@@ -376,7 +376,10 @@ if st.session_state.page == 'main':
     if 'selected_regions' not in st.session_state:
         st.session_state.selected_regions = []
 
-    
+    # 선택 초기화 버튼 클릭 시 선택된 지역 초기화
+    if st.button("선택 초기화"):
+        selected_region = None
+        st.session_state.selected_regions = []
         
     # 선택된 지역 텍스트를 위한 placeholder 생성
     selected_region_text = st.empty()
@@ -475,9 +478,7 @@ if st.session_state.page == 'main':
             unsafe_allow_html=True
         )
         
-    # 선택 초기화 버튼 클릭 시 선택된 지역 초기화
-    if st.button("선택 초기화"):
-        st.session_state.selected_regions = []
+    
         
     st.markdown('<div class="spacing-100px"></div>', unsafe_allow_html=True)
         
