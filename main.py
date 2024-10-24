5dabf19709ec540e29e2136861b7a7a8150efbaa
Gina Kim <gina261@yonsei.ac.kr>
2024-10-24 22:50:49 +0900

    1024

--- main.py
+++ main.py
@@ -743,6 +743,7 @@ elif st.session_state.page == 'next_page':
         
     def go_to_previous():
         st.session_state.page = 'main'
+        st.experimental_rerun()
         
     if st.button("⇦ 뒤로"):
         go_to_previous()
