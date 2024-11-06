a6ae33b60792f61ae66dd40913acc224b46d2cdf
Gina Kim <gina261@yonsei.ac.kr>
2024-11-07 03:15:35 +0900

    edited: sidebar

--- main.py
+++ main.py
@@ -865,6 +865,6 @@ elif st.session_state.page == 'next_page':
     # 인원수 확인 및 수정
     st.sidebar.subheader("인원")
     if st.session_state.members_num == "":
-        st.write("HI")
+        st.session_state.members_num = 
         
     st.write("인원수", st.session_state.members_num)
