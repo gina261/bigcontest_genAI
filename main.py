3e2332cec65bb03cabe9973190841d9a4aaba79f
Gina Kim <gina261@yonsei.ac.kr>
2024-11-11 13:19:29 +0900

    edited: expander border

--- main.py
+++ main.py
@@ -800,7 +800,7 @@ elif st.session_state.page == 'next_page':
         
         .expander-border {
             position: fixed;
-            top: 155px;
+            top: 153px;
             left: 22px;
             width: 130px;
             height: 35px;
